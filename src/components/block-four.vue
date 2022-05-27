30211e8555cae9a94d865d252b8e3de852fcd3c0
banahaker <pn0818x@outlook.com>
2022-05-27 10:22:31 +0800

    feat:  page four i18n

--- src/components/block-four.vue
+++ src/components/block-four.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup></script>
 <template>
-  <div class="blockFour"></div>
+  <div class="blockFour">
+    <div class="contain">
+      <div class="text">
+        <h1></h1>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss"></style>
