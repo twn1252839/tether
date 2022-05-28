3259eda745c87d36afe53dd439eb9e26c41924dd
banahaker <pn0818x@outlook.com>
2022-05-28 14:50:36 +0800

    feat: page five

--- src/components/block-five.vue
+++ src/components/block-five.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
 import { reactive } from "vue";
 import { useI18n } from "vue-i18n";
+import illus1 from "../assets/illus1.png";
+import illus2 from "../assets/illus2.png";
+import illus3 from "../assets/illus3.png";
 const { t } = useI18n();
 
 interface card {
@@ -12,17 +15,17 @@ interface card {
 const cardsContent = reactive<Array<card>>([
   {
     title: "blockFive.card.1.title",
-    svgSource: "card1",
+    svgSource: illus1,
     content: "blockFive.card.1.content",
   },
   {
     title: "blockFive.card.2.title",
-    svgSource: "card2",
+    svgSource: illus2,
     content: "blockFive.card.2.content",
   },
   {
     title: "blockFive.card.2.title",
-    svgSource: "card3",
+    svgSource: illus3,
     content: "blockFive.card.2.content",
   },
 ]);
@@ -39,8 +42,9 @@ const cardsContent = reactive<Array<card>>([
         <div class="card" v-for="card in cardsContent">
           <div class="titleBar">
             <h2>{{ t(card.title) }}</h2>
+            <img :src="card.svgSource" alt="illus" />
           </div>
-          <span> {{ t(card.content) }}</span>
+          <p>{{ t(card.content) }}</p>
         </div>
       </div>
     </div>
@@ -49,24 +53,88 @@ const cardsContent = reactive<Array<card>>([
 </template>
 
 <style lang="scss">
+@import "../scss/global.scss";
 .blockFive {
   display: flex;
   flex-direction: row;
+  width: 100vw;
+  .block {
+    flex-grow: 1;
+  }
   .contain {
-    padding: 20px;
+    max-width: 1440px;
+    width: 1440px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    overflow: hidden;
+    flex-grow: 1;
+    max-width: 1440px;
+    align-items: center;
     .title {
-      margin-bottom: 50px;
+      max-width: 570px;
+      text-align: center;
+      margin: 50px 10px;
       h1 {
+        color: #232323;
         font-size: 1.875rem;
-        line-height: 1.875rem;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 36px;
+        @include phone {
+          font-size: 1.375rem;
+        }
       }
       & > .cards {
         display: flex;
         width: 100%;
         overflow-x: auto;
+      }
+    }
+    .cards {
+      margin: 10px;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      width: 100%;
+      height: fit-content;
+      padding-bottom: 24px;
+      overflow-x: auto;
+      overflow-y: hidden;
+      align-items: center;
+      justify-content: center;
+      .card {
+        padding: 40px;
+        width: 380px;
+        color: #232323;
+        transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+        background-color: #fff;
+        border-radius: 6px;
+        flex-grow: 0;
+        &:not(:first-child) {
+          margin-left: 20px;
+        }
+        .titleBar {
+          display: flex;
+          align-items: flex-start;
+          margin-bottom: 16px;
+          flex-direction: row;
+          justify-content: space-between;
+
+          h2 {
+            color: #009393;
+            font-size: 1.875rem;
+            font-style: normal;
+            font-weight: 700;
+            line-height: 36px;
+          }
+        }
+        p {
+          color: #586871;
+          font-size: 0.875rem;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 25px;
+        }
       }
     }
   }
